07e3a33036d2e1c75c40df3a498627c9bbe96b37
sssazer <wangyizesaszer@163.com>
2023-03-09 15:00:58 +0800

    添加程序入口main函数所在文件： main文件

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include "ManageSystem.h"
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	ManageSystem ms;
+	ms.mainPage();
+
+	//char* test = new char[5];
+	//cin >> test;
+	//cout << test;
+	//delete test;
+	return 0;
+}
